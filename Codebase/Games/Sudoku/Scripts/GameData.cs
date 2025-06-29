91afbe437474c4bb8505775474bf0c24b2ceb165
☆ Melody H. Song ☆ <55931729+MelodyHSong@users.noreply.github.com>
2025-06-28 23:33:50 -0400

    Create GameData.cs

---
+++ Codebase/Games/Sudoku/Scripts/GameData.cs
@@ -0,0 +1,40 @@
+//============================================================================
+//	Author: ✰ @MelodyHSong ✰
+//	Date: 2025-06-28
+//	Project: 10TinyBuilds-Sudoku
+//  Description: GameData.cs
+//============================================================================
+
+// This script defines the data structures used for saving game state and high scores in a Sudoku game.
+
+using System.Collections.Generic;
+
+// Ignore Spelling: Sudoku
+
+// * A class to hold all the data for a saved game state *
+[System.Serializable]
+public class GameData
+{
+    public int[,] board;
+    public int[,] solvedBoard;
+    public int[,] initialPuzzle;
+    public bool[,] mistakesMade;
+    public int score;
+    public int difficulty;
+}
+
+// * A class for a single high score entry *
+[System.Serializable]
+public class HighScore
+{
+    public int score;
+    public string status; // * "Completed" or "Gave Up" *
+    public string date;
+}
+
+// * A class to hold a list of all high scores *
+[System.Serializable]
+public class HighScoresData
+{
+    public List<HighScore> highScores = new List<HighScore>();
+}
